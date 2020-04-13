dea90fe80cdcb2f2054637426d97f273cd0f1ac1
Robert Potts <robert@pottsy.me.uk>
2020-04-14 00:00:00 +0100

    Added support for upload page.

--- NewDash/UpdatedDash.php
+++ NewDash/UpdatedDash.php
@@ -42,6 +42,7 @@ include ('../common/head.php');
 <script src="app/products/read-one-product.js"></script>
 <script src="app/products/update-product.js"></script>
 <script src="app/products/delete-product.js"></script>
+<script src="app/products/upload-product.js"></script>
 
 </body>
 </html>
