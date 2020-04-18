46f1d78fcb275ac04ad8ebb0a73ef4859db0b72f
Robert Potts <robert@pottsy.me.uk>
2020-04-18 12:49:32 +0100

    Login Page Change

--- api/shootrecords/CSVRegionalExporter.php
+++ api/shootrecords/CSVRegionalExporter.php
@@ -9,7 +9,7 @@ $db = $database->getConnection();
 
 
 $result = "SELECT a.*, display_name FROM shootrec a, registered_users
-            WHERE region ='$Region' ";
+            WHERE region ='$Region' AND a.AGBNo = registered_users.agb_no ";
 //$shrecs = $db->query($result);
 
 $stmt = $db->prepare($result);
