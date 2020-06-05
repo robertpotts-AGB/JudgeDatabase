d83f3b4f39f49d7cd461b16ecd44125459fbdc3a
Robert Potts <robert@pottsy.me.uk>
2020-06-05 16:37:51 +0100

    Dependant Menus

--- NewDash/app/products/create-product.js
+++ NewDash/app/products/create-product.js
@@ -13,12 +13,19 @@ $(document).ready(function(){
             categories_options_html+=`</select>`;
             // we have our html form here where shootrecords information will be entered
 // we used the 'required' html5 property to prevent empty fields
+            window.addEventListener( "pageshow", function ( event ) {
+                var historyTraversal = event.persisted || ( typeof window.performance != "undefined" && window.performance.navigation.type === 2 );
+                if ( historyTraversal ) {
+                    window.location.reload();
+                }
+            });
             var create_product_html=`
  
     <!-- 'read products' button to show list of products -->
     <div id='read-products' class='btn btn-primary pull-right m-b-15px read-products-button'>
         <span class='glyphicon glyphicon-list'></span> My Shoots
     </div>
+   
     <!-- 'create shootrecords' html form -->
 <form id='create-shootrecords-form' action='#' method='post' border='0'>
     <table class='table table-hover table-responsive table-bordered'>
@@ -34,40 +41,30 @@ $(document).ready(function(){
             <td><input type='text' name='EvName' class='form-control' required /></td>
         </tr>
         <tr>
-            <td>Shoot Round</td>
-            <td><select name='EvRound' class='form-control' required >
-                <option value="">...Select</option>
-                <option value="OnTarget">Ontarget</option>
-                <option value="1440/Metrics">1440 and Metrics I-V</option>
-                <option value="2x1440/Metrics">Double 1440 and Metrics I-V</option>
-                <option value="720">70/60/50 Meter round</option>
-                <option value="2x720"> Double 70/60/50 Meter round</option>
-                <option value="York/Hereford/Bristols">York/Hereford/Bristols</option>
-                 <option value="Nationals">Nationals</option>
-                <option value="Metrics">Metrics I-V</option>
-                <option value="American">American</option>
-                <option value="Albion/Windsors">St George/Albion/Windsors</option>
-                <option value="Stafford">Stafford</option>
-                <option value="Vegas">Vegas</option>
-                <option value="Short Metrics">Short Metrics</option>
-                <option value="Long Metrics">Long Metrics</option>
-                <option value="Worcester">Worcester</option>
-                <option value="WA18">WA 18 Meter Round</option>
-                <option value="WA25">WA 25 Meter Round</option>
-                <option value="Portsmouth">Portsmouth</option>
-                <option value="Training">Training</option>
-              </select></td>
-        </tr>
-        <!-- price field -->
-        <tr>
-            <td>Shoot Rules</td>
-             <td><select name='EvOrg' class='form-control' required>
+            <td>Shoot Rules  </td>
+             <td><select onChange="populateType('evrules','evtypes');" id="evrules" name='EvOrg' class='form-control' required>
                 <option value="">...Select</option>
                 <option value="AGB">AGB</option>
                 <option value="WA">WA</option>
                 <option value="Training">Training</option>
               </select></td>
         </tr>
+         <tr>
+            <td>Event Type</td>
+            <td><select id="evtypes"  onChange="populateRound('evrules','evtypes','evround');" name='EvDiscipline' class='form-control' required>
+                <option id="select" value="">...Select</option>
+             
+                </select></td>
+        </tr>
+        <tr>
+            <td>Shoot Round</td>
+            <td><select name='EvRound'  id="evround" class='form-control' required >
+                <option id="select1" selected value="">...Select</option>
+           
+              </select></td>
+        </tr>
+        <!-- price field -->
+        
  
         <!-- description field -->
        <tr>
@@ -81,21 +78,7 @@ $(document).ready(function(){
                 <option value="Club">Club</option>
               </select></td>
         </tr>
-          <tr>
-            <td>Event Type</td>
-            <td><select name='EvDiscipline' class='form-control' required>
-                <option value="">...Select</option>
-                <option value="TGTOutdoor">Target Outdoor</option>
-                <option value="TGTIndoor">Target Indoor</option>
-                <option value="Field">Field</option>
-                <option value="3D">3D</option>
-                <option value="Clout">Clout</option>
-                <option value="Flight">Flight</option>
-                <option value="Seminar">Seminar</option>
-                <option value="Conference">Conference</option>
-                <option value="Other CPD">Other CPD</option>
-                </select></td>
-        </tr>
+         
           <tr>
             <td>Optional Event Information</td>
             <td><select name='EvOptional' class='form-control' >
@@ -140,7 +123,113 @@ $(document).ready(function(){
         </tr>
  
     </table>
-</form>`;
+</form>
+
+  
+<script>
+                function populateType(s1,s2){
+                    var s = document.getElementById(s1);
+                    var s_a = document.getElementById(s2);
+
+
+                    var select = document.getElementById(s2).options.length;
+                    for (var i = select; i >0;i-- ) {
+                        document.getElementById(s2).options.remove(i);
+                        console.log(i);
+                    }
+                    document.getElementById('select').selected='selected';
+                    var optionArray=[];
+                    if(s.value == "Training"){
+                        optionArray = ["Seminar|Seminar","Conference|Conference","Other CPD|Other CPD"];
+                    }
+                    else if(s.value == "AGB"){
+                        optionArray = ["TGTOutdoor|Target Outdoor","TGTIndoor|Target Indoor","Field|Field","3D|3D","Clout|Clout","Flight|Flight"];
+                    }
+                    else if(s.value == "WA"){
+                        optionArray = ["TGTOutdoor|Target Outdoor","TGTIndoor|Target Indoor","Field|Field","3D|3D","Clout|Clout","Flight|Flight"];
+                    }
+                    for(var option in optionArray){
+                        var pair = optionArray[option].split("|");
+                        var newOption = document.createElement("option");
+                        newOption.value = pair[0];
+                        newOption.innerHTML = pair[1];
+                        s_a.options.add(newOption);
+                    }
+                }
+            function populateRound(s1,s2,s3){
+                    var evrule = document.getElementById(s1);
+                    var evtype = document.getElementById(s2);
+                    var evround = document.getElementById(s3);
+
+
+                    var select = document.getElementById(s3).options.length;
+                    for (var i = select; i >0;i-- ) {
+                        document.getElementById(s3).options.remove(i);
+                        console.log(i);
+                    }
+                    document.getElementById('select1').selected='selected';
+                    var optionArray=[];
+                    if(evrule.value =="WA")
+                        {
+                    if(evtype.value == "TGTOutdoor"){
+                        optionArray = ["720|WA 70/60/50","2x720|Double WA 70/60/50" ,"WA1440|1440 and Metrics I-V","2x1440/Metrics|Double 1440 and Metrics I-V","900|WA900"];
+                    }
+                    else if(evtype.value == "TGTIndoor"){
+                        optionArray = ["WA18|WA 18","WA25|WA 25","Double18|Double 18m","Double25|Double 25m"];
+                    }
+                       else if(evtype.value == "Field"){
+                        optionArray = ["Unmarked|Unmarked","Marked|Marked","12+12|12+12","24+24|24+24"];
+                    }
+                      else if(evtype.value == "3D"){
+                        optionArray = ["3D|3D"];
+                    }
+                      else if(evtype.value == "Clout"){
+                        optionArray = ["1xClout|One Way Clout","2xClout|Two Way Clout"];
+                    }
+                          else if(evtype.value == "Flight"){
+                        optionArray = ["Flight|Flight"];
+                    }
+                  }
+                    else if(evrule.value == "AGB")
+                    { 
+                        if(evtype.value == "TGTOutdoor"){
+                        optionArray = ["York/Hereford/Bristols|York/Hereford/Bristols","Nationals|Nationals","Metrics|Metrics I-V","American|American","Albion/Windsors|St George/Albion/Windsors","Short Metrics|Short Metrics","Long Metrics|Long Metrics"];
+                    }
+                    else if(evtype.value == "TGTIndoor"){
+                        optionArray = ["Stafford|Stafford","Portsmouth|Portsmouth","Bray1|Bray 1","Bray2|Bray 2","Worcester|Worcester","Vegas|Vegas"];
+                    }
+                      else if(evtype.value == "Field"){
+                        optionArray = ["Unmarked|Unmarked","Marked|Marked","12+12|12+12","24+24|24+24"];
+                    }
+                      else if(evtype.value == "3D"){
+                        optionArray = ["3D|3D"];
+                    }
+                      else if(evtype.value == "Clout"){
+                        optionArray = ["1xClout|One Way Clout","2xClout|Two Way Clout"];
+                    }
+                          else if(evtype.value == "Flight"){
+                        optionArray = ["Flight|Flight"];
+                    }
+                   
+                    }
+                     else if(evrule.value == "Training")
+                    {
+                         optionArray = ["Training|Training"];
+                    }
+                     
+                    for(var option in optionArray){
+                        var pair = optionArray[option].split("|");
+                        var newOption = document.createElement("option");
+                        newOption.value = pair[0];
+                        newOption.innerHTML = pair[1];
+                        evround.options.add(newOption);
+                    }
+                }
+                    
+            </script>
+
+`
+            ;
             // inject html to 'page-content' of our app
             $("#page-content").html(create_product_html);
 
