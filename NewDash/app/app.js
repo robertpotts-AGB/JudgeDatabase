453cba3808feb6cab25001b390fea3a4c60be559
Robert Potts <robert@pottsy.me.uk>
2021-01-09 12:28:54 +0000

    Dependant Menus

--- NewDash/app/app.js
+++ NewDash/app/app.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
                 <h1 id='page-title' class="card card-body bg-light" >My Shoots</h1>
             </div>
             <div id='page-content'></div>
-            <canvas id="myChart" width="400" height="400"></canvas>
+            <canvas id="myChart" width="150" height="150"></canvas>
 
             <div class='container'>
             <!-- this is where the contents will be shown. -->
