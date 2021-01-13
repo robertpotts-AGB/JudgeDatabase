25d92c2fa9a3b44ce3207aa4dc3f3807be4f44d2
Robert Potts <robert@pottsy.me.uk>
2021-01-13 11:33:00 +0000

    Dependant Menus

--- NewDash/app/products/yearchange.php
+++ NewDash/app/products/yearchange.php
@@ -4,5 +4,5 @@ session_start();
 if (isset($_POST['year'])) {
     // save values from other page to session
     $_SESSION['ShYear'] = $_POST['year'];
-    header("location:/JudgeDatabase/NewDash/UpdatedDash.php");
+
 }
