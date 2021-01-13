d42a2b0a7e209e223e33fa7a2fdd79fb28e3754e
Robert Potts <robert@pottsy.me.uk>
2021-01-13 12:48:35 +0000

    Dependant Menus

--- NewDash/app/products/RegionStats.js
+++ NewDash/app/products/RegionStats.js
@@ -8,10 +8,67 @@ $(document).ready(function(){
         changePageTitle("Statistics");
         $("#page-content").html("");
     });
+    $(document).on('click', '.NationalStats-button', function(){
+        showNatStats();
+
+        changePageTitle("National Statistics");
+        $("#page-content").html("");
+    });
 });
 
 // function to show list of products
 function showJLOStats() {
+// get list of products from the API
+    $.post("/JudgeDatabase/api/shootrecords/GraphCalcs.php", function (data) {
+// html for listing products
+        console.log(data);
+        var evorg = [];
+        var orgtotal = [];
+
+        for (var i in data) {
+            evorg.push(data[i].EvOrg);
+            orgtotal.push(data[i].OrgTotal);
+        }
+
+        var chartdata = {
+            labels: evorg,
+            datasets: [
+                {
+                    label: 'Organisation',
+                    backgroundColor: '#49e2ff',
+                    borderColor: '#46d5f1',
+                    hoverBackgroundColor: '#CCCCCC',
+                    hoverBorderColor: '#666666',
+                    data: orgtotal
+                }
+            ]
+        };
+
+        var chartoptions={
+            responsive: false,
+            scales: {
+                yAxes:[{
+                    max: 300,
+                    min: 50,
+                    ticks:{
+                        stepSize:50
+                    }
+                }]
+            }
+
+        };
+
+        var graphTarget = $("#myChart");
+
+        var barGraph = new Chart(graphTarget, {
+            type: 'bar',
+            data: chartdata,
+            options:chartoptions
+
+        });
+    });
+}
+function showNatStats() {
 // get list of products from the API
     $.post("/JudgeDatabase/api/shootrecords/GraphCalcs.php", function (data) {
 // html for listing products
