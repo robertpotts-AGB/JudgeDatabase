dd2665e433bbc36494bcd854aed0d1606e2c9489
Robert Potts <robert@pottsy.me.uk>
2021-01-31 21:58:11 +0000

    Dependant Menus

--- NewDash/UpdatedDash.php
+++ NewDash/UpdatedDash.php
@@ -125,7 +125,7 @@ if(!isset($_SESSION['username'])){
             <select name="CurrYear" onchange="YearChange(this.value)">
                 <option value="<?php echo $_SESSION["ShYear"] ?>"><?php echo $_SESSION["ShYear"] ?></option>
                 <?php
-                for($i = 2010 ; $i < date('Y'); $i++){
+                for($i = date('Y') ; $i = 2010; $i--){
                     echo "<option>$i</option>";
                 }
                 ?>
